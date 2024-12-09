72a6d9af9e98d1e0b94c2eca4c4807f246d028e7
ecourse <smkang@codenuri.co.kr>
2024-12-09 13:46:28 +0900

    Add files via upload

---
+++ DAY1/13_default_function.cpp
@@ -0,0 +1,28 @@
+﻿// 13_default_function - 41 page
+#include <type_traits>
+#include <iostream>
+
+class Point
+{
+	int x, y;
+public:
+	Point(int a, int b) {}
+
+	// 인자가 없는 생성자가 필요할때
+	// 생성자를 만드는 2가지 방법
+	// #1. 사용자가 직접 구현부를 제공
+	// Point() {} 
+
+	// #2. 컴파일러에게 만들어 달라고 요청
+	Point() = default;
+};
+
+int main()
+{
+	Point p1;
+	Point p2(1, 2);
+}
+
+
+
+
