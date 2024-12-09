0103067660ca57dd8d87b36c2cba2d56856abdf3
ecourse <smkang@codenuri.co.kr>
2024-12-09 15:55:05 +0900

    Add files via upload

--- DAY1/19_structure_binding.cpp
+++ DAY1/19_structure_binding.cpp
@@ -30,7 +30,18 @@ int main()
 
 	auto [x3, y3, _] = p3d; // ok.  주의할 점은 _ 도 변수 이름이라는 것
 
-	auto [x4, y4, _] = p3d; // error. 이미 선언된 _ 변수를 다시한번 선언한것
+//	auto [x4, y4, _] = p3d; // error. 이미 선언된 _ 변수를 다시한번 선언한것
+							// 단, C++26 부터는 에러 아님. 
+				
+	// C++26 부터, 재미있는 문법한개가 추가되었습니다.
+	// 이름이 _ 인 지역변수는 여러번 선언될수 있다.
+//	int _ = 10;
+//	int _ = 20; // C++26 부터 에러 아님.
+
+	// 구조체 뿐 아니라 배열도 가능합니다.
+	int arr[3] = { 1,2,3 };
+
+	auto [a, b, c] = arr; // 갯수 맞아야 합니다.
 
 
 
@@ -41,10 +52,28 @@ int main()
 	m["mon"] = "월요일";
 
 	// map은 pair를 보관합니다.
+	// 위 한줄은 결국 아래 4줄과 같은 코드 입니다.
 	std::pair<std::string, std::string> p;
+
 	p.first = "tue";
 	p.second = "화요일";
 	m.insert(p);
 
+	// 핵심 map 의 모든 요소를 "range-for"로 열거 하는 방법
+	// C++17 이전
+	for (auto p : m)
+	{
+		// 여기서 p 는 std::pair 입니다.
+		std::string key = p.first;
+		std::string value = p.second;
+	}
+
+
+	// C++17 부터는 위 structure binding 을 사용하면 편리합니다.
+	// => 아래 코드가 위코드와 동일합니다.
+	for (auto[key, value] : m)
+	{
+		
+	}
 }
 
