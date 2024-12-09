33aae1046bef77ef439b72d38a7e22bb266b6ac6
ecourse <smkang@codenuri.co.kr>
2024-12-09 17:16:15 +0900

    Add files via upload

---
+++ DAY1/20_초기화10.cpp
@@ -0,0 +1,30 @@
+﻿// 1_초기화10
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	std::vector<int> v1 = { 1,2,3,4,5 };
+	std::vector<int> v2{ 1,2,3,4,5 };
+
+
+	// 아래 2줄의 차이점은 ?
+	std::vector<int> v3(10, 2); // 10개 요소를 2로 초기화. v3.size() 는 10
+								// vector(int, int) 생성자
+
+	std::vector<int> v4{10, 2};	// 2개를 10, 2 로 초기화. v4.size() 는 2
+								// vector(std::initializer_list<int>)
+
+
+	// 아래 코드에서 에러는 ?
+	std::vector<int> v5 = 10;	// error. 
+								// vector(int) 생성자는 explicit 이므로!!
+
+	std::vector<int> v6 = {10}; // ok
+								// vector(std::initializer_list<int>) 생성자는
+								// explicit 가 아닙니다.
+}
+
+
+
+
