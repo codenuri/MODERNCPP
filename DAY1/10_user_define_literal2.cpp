75de02ede5ba64dff75c432a35646be171e09497
ecourse <smkang@codenuri.co.kr>
2024-12-09 13:32:36 +0900

    Add files via upload

--- DAY1/10_user_define_literal2.cpp
+++ DAY1/10_user_define_literal2.cpp
@@ -12,6 +12,13 @@ int main()
 	foo("hello");	// 1. 정확히는 const char[6] 인데, const char* 로 변환 가능
 	foo("hello"s);  // 2. std::string operator""s(const char*) 가 호출된것
 		
+
+//	foo("hello"std::liternals::s); // error
+
+	foo(std::literals::operator""s("hello", 6)); // ok 
+
+
+
 //	std::this_thread::sleep_for( 3 ); // 현재 스레드를 특정 시간동안 재우기
 									  // 인자로는 반드시 초, 분, 시간등의 타입만 가능.
 
@@ -30,6 +37,7 @@ int main()
 										// 그런데, 이코드는 요소의 타입이 const char*
 
 	std::vector v3 = { "AA"s, "BB"s };  // ok. 요소의 타입이 std::string
+
 }
 
 
