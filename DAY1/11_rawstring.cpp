5ad3b04742b2a31a3911053327b16d5bdd52d98b
ecourse <smkang@codenuri.co.kr>
2024-12-09 13:35:21 +0900

    Add files via upload

---
+++ DAY1/11_rawstring.cpp
@@ -0,0 +1,17 @@
+﻿// 11_rawstring
+#include <iostream>
+#include <string>
+
+int main()
+{
+
+	// '\' 문자 : 특수 문자 출력시 사용
+	//           '\' 자체를 표현하려면 '\\' 로 해야
+	std::string s1 = "\\\\.\\pipe\\server";
+
+	// Raw String : '\' 를 특수 문자가 아닌 일반 문자열로 처리.
+	std::string s2 = R"(\\.\pipe\server)";
+
+	std::cout << s1 << std::endl;
+	std::cout << s2 << std::endl;
+}
