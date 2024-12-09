fa371848ca27472945d1063859e98fa720a0b867
ecourse <smkang@codenuri.co.kr>
2024-12-09 12:02:21 +0900

    Add files via upload

---
+++ DAY1/10_user_define_literal1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+class Meter
+{
+	int value;
+public:
+	Meter(int v) : value(v) {}
+};
+
+// 아래 함수를 "사용자 정의 리터럴 접미사" 를 만드는 코드 입니다.
+Meter operator""m(int n)
+{
+	Meter met(n);
+	return met;
+}
+
+
+int main()
+{
+	Meter met = 3m; // operator""m(3) 을 호출하기로 약속되어 있습니다.
+
+
+//	float f = 3.4;  // 3.4 만 표시하면 double 타입
+//	float f = 3.4f; // 이렇게 하면 float 타입의 3.4
+}
+
+
+
+
