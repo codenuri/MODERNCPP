23bc3bd278b602511663fba17d2c94b5231dd651
ecourse <smkang@codenuri.co.kr>
2024-12-09 11:53:43 +0900

    Add files via upload

--- DAY1/9_enum2.cpp
+++ DAY1/9_enum2.cpp
@@ -13,10 +13,19 @@ int main()
 //	int n2 = COLOR::red;	// error. int 타입이 아닌 COLOR 타입
 							//        int 타입으로 암시적 변환 안됨
 	COLOR c1 = COLOR::red;  // ok
-	COLOR c2 = red;			// error. COLOR 필요
+//	COLOR c2 = red;			// error. COLOR 필요
+
+	// 캐스팅하면 int 타입으로 변환 가능합니다.
+	int n = static_cast<int>(COLOR::red);
 }
 
+// C++98 시절 함수
+void foo(int color) {}
+foo(red);	// ok. red는 enum 상수
+foo(-100);	// 잘못 사용했지만 에러 아님.
 
 
-
-
+// C++11 이후
+void foo(COLOR c) {}
+foo(COLOR::red); // ok
+foo(100); // error. int 는 COLOR 타입 아님.
