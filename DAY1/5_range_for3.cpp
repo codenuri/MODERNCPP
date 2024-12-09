4bf49dfc07cab3fe3b7e49ca44bc333c08af40bf
ecourse <smkang@codenuri.co.kr>
2024-12-09 10:47:25 +0900

    Add files via upload

--- DAY1/5_range_for3.cpp
+++ DAY1/5_range_for3.cpp
@@ -18,8 +18,10 @@ int main()
 {
 	std::vector<int> v = { 1,2,3,4,5,6,7,8,9,10 };
 	
-	take_view< std::vector<int> > tv(v, 3);
-
+//	take_view< std::vector<int> > tv(v, 3); // v의 요소를 3개만 볼수 있는 view
+	take_view tv(v, 3); // C++17 부터 템플릿 인자 생략가능. 
+						// => 프로젝트 메뉴
+						// => DAY1 속성메뉴에서 C++ 버전을 C++17 로 맞추어야 합니다.
 	for (int e : tv)
 	{
 		std::cout << e << ", ";
