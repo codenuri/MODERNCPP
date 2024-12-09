7cb75774dd4b131b1ad3a0be89b708cf5376b11d
ecourse <smkang@codenuri.co.kr>
2024-12-09 12:08:55 +0900

    Add files via upload

--- DAY1/10_user_define_literal1.cpp
+++ DAY1/10_user_define_literal1.cpp
@@ -8,16 +8,23 @@ public:
 };
 
 // 아래 함수를 "사용자 정의 리터럴 접미사" 를 만드는 코드 입니다.
-Meter operator""m(int n)
+// => 함수 인자 타입이 약속되어 있습니다. ( 34 page 아래 표)
+// => 정수형 리터럴 접미사는 "unsigned long long" 으로 해야 합니다.
+
+// 또한
+// _ 로 시작되지 않은 이름은 "표준에서만 사용하기로 약속" 되어 있고
+// 개발자가 직접 만드는 것은 반드시 _로 시작해야 합니다.
+// => 그런데, 만들지는 말고, 표준것만 사용하세요.. 
+Meter operator""_m(unsigned long long n)
 {
-	Meter met(n);
+	Meter met( static_cast<int>(n) );
 	return met;
 }
 
 
 int main()
 {
-	Meter met = 3m; // operator""m(3) 을 호출하기로 약속되어 있습니다.
+	Meter met = 3_m; // operator""m(3) 을 호출하기로 약속되어 있습니다.
 
 
 //	float f = 3.4;  // 3.4 만 표시하면 double 타입
