7898323ad7b3a16f2cf8c1b94d041b72bb93112e
ecourse <smkang@codenuri.co.kr>
2024-12-09 09:46:17 +0900

    Add files via upload

---
+++ DAY1/2_using1.cpp
@@ -0,0 +1,16 @@
+﻿// 11 page
+// 
+// C 스타일 코드 - typedef 
+// typedef int DWORD;
+// typedef void(*F)(); 
+
+// 아래 2줄이 위 2줄과 완전히 동일합니다.
+// C++11 using alias
+using DWORD = int;
+using F = void(*)();
+
+int main()
+{
+	DWORD n;	// int n
+	F     f;	// void(*f)();
+}
