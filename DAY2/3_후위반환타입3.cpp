fc1c10e87e0078ca5cd40960349c7e9311750666
ecourse <smkang@codenuri.co.kr>
2024-12-10 11:04:44 +0900

    Add files via upload

---
+++ DAY2/3_후위반환타입3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+template<typename T1, typename T2>
+auto  Mul1(T1 a, T2 b) -> decltype(a* b) 
+{
+	return a * b;
+}
+
+template<typename T1, typename T2>
+auto  Mul2(T1 a, T2 b)
+{
+	return a * b;
+}
+
+int main()
+{
+	std::cout << Mul1(3, 4.1) << std::endl;
+	std::cout << Mul2(3, 4.1) << std::endl;
+
+}
+
