b8d421ead1d94596d57d0fc578de16aa8b558f1f
ecourse <smkang@codenuri.co.kr>
2024-12-10 09:23:27 +0900

    Add files via upload

--- DAY2/2_타입추론2-1.cpp
+++ DAY2/2_타입추론2-1.cpp
@@ -7,7 +7,15 @@
 
 template<typename T> void foo(T a)
 {
-	a = 100;
+	// __func__ : 함수 이름을 담은 매크로 (C++표준)
+	//			  인자 없이 함수 이름만..
+
+	// __FUNCSIG__ : 함수 이름 + 함수 인자 (C++ 표준아님. MS 컴파일러만 지원)
+	// __PRETTY_FUNCTION__ : g++, clang++ 만 지원
+
+	//std::cout << __func__ << std::endl;
+	std::cout << __FUNCSIG__ << std::endl;
+	
 }
 
 int main()
