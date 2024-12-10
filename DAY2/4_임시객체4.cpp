4bc74b2b12d7c5aa1899b4c48c50a9fd1fe2ef42
ecourse <smkang@codenuri.co.kr>
2024-12-10 13:33:24 +0900

    Add files via upload

---
+++ DAY2/4_임시객체4.cpp
@@ -0,0 +1,44 @@
+﻿// 3_임시객체1 - 77 page
+#include <iostream>
+
+class Point
+{
+public:
+	int x, y;
+
+	Point(int a, int b) { std::cout << "Point()" << std::endl; }
+	~Point() { std::cout << "~Point()" << std::endl; }
+};
+// 임시객체와 함수 반환값 -  82 page
+/*
+void f1(Point  pt) {}	// call by value     : 인자의 복사본 생성
+void f2(Point& pt) {}	// call by reference : 복사본 생성안됨
+
+
+int main()
+{
+	Point p(0, 0);
+	f1(p);
+	f2(p);
+}
+*/
+
+
+
+
+//--------------------
+Point pt(1, 2);
+
+Point foo() 
+{
+	return pt; 
+}
+
+int main()
+{
+	foo().x = 10;
+}
+
+
+
+
