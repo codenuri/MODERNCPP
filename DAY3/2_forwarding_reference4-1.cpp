10c4475ebf96aea630d45d9082473a752838f852
ecourse <smkang@codenuri.co.kr>
2024-12-11 11:54:22 +0900

    Add files via upload

--- DAY3/2_forwarding_reference4-1.cpp
+++ DAY3/2_forwarding_reference4-1.cpp
@@ -21,4 +21,17 @@ fn(n), lvalue 전달 : T = int&, T&&=int& && => int&   fn(int&)   함수 생성
 fn(3), rvalue 전달 : T = int,  T&&=int&&             fn(int&&)  함수 생성
 
 
-// 
+// 용어 정리
+int&  : lvalue reference
+int&& : rvalue reference
+T&    : lvalue reference
+
+T&&   : lvalue, rvalue 모두 가리킬수 있으므로 "rvalue reference" 아닙니다.
+		새로운 용어가 필요 한데, C++11 만들때 용어를 정하지 않았습니다. 
+
+		effective-c++ 책을 쓴 scott-meyer 사람이 아래 용어를 제안 
+	    "universal reference"
+		
+		그런데, 표준위원회는 나중에 아래의 용어로 결정
+		"forwarding reference"
+		=> 실제 이기술은 "perfect forwarding" 할때 사용하기 때문에 붙인 이름
