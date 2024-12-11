db1dbc087b57214392a50bcae5ef92f7d1dee142
ecourse <smkang@codenuri.co.kr>
2024-12-11 10:16:37 +0900

    Add files via upload

--- DAY3/1_move13.cpp
+++ DAY3/1_move13.cpp
@@ -19,7 +19,20 @@ public:
 	// 방법 2. call by const reference
 	// => C++98 시절이라면 최선의 코드!!!
 	// => C++11 이후부터는 "최선은 아님"
+	// => 아래 코드는 이동을 지원할수 없는 setter
+	// p.set_name(s1);				
+	// p.set_name(std::move(s2)); // 이렇게 사용해도 복사 됩니다.
+//	void set_name(const std::string& n) { name = n; } // 항상 복사 되는 코드
+
+	// 아래 처럼 만들면 어떨까요 ?
+	// => 인자 n 이 const 이므로 "역시 항상 복사" 입니다.
+//	void set_name(const std::string& n) { name = std::move(n); } // 항상 복사
+
+
+	// 방법 3. 정답!!!
+	// => move 를 지원하는 setter를 만들려면 "2개" 를 만들어라!
 	void set_name(const std::string& n) { name = n; }
+	void set_name(std::string&& n)      { name = std::move(n); }
 };
 
 int main()
@@ -29,8 +42,8 @@ int main()
 
 	People p;
 
-	p.set_name(s1);
-	p.set_name(std::move(s2));	
+	p.set_name(s1);				// s1의 자원(문자열)을 복사해 가라는 것
+	p.set_name(std::move(s2));	// s2의 자원(문자열)을 이동해 가라는 것
 
 	std::cout << s1 << std::endl; // "kim"
 	std::cout << s2 << std::endl; // ""
