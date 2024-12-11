9235625312c6629145186e3adf5dbb340dcfc357
ecourse <smkang@codenuri.co.kr>
2024-12-11 16:57:02 +0900

    Add files via upload

---
+++ DAY3/6_함수객체4.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+bool foo(int n) { return n % 3 == 0; }
+
+int main()
+{
+	std::vector<int> v = { 1,2,6,5,4,3,7,8 };
+
+	int k = 3; // 사용자가 입력한 값이라고 가정
+
+	// 주어진 구간에서 "k의 배수" 를 찾고 싶다 ???
+	// => 어떻게 해야 할까요 ?
+	auto p1 = std::find_if(v.begin(), v.end(), foo);
+
+}
+
+
+
