ec646e3f8c5940138081a4c78c2a861639aa4e52
ecourse <smkang@codenuri.co.kr>
2024-12-11 09:43:42 +0900

    Add files via upload

--- DAY3/1_move10-1.cpp
+++ DAY3/1_move10-1.cpp
@@ -3,6 +3,9 @@
 
 // 자동 생성 규칙.
 // 규칙 #2. 사용자가 복사생성자만 제공한 경우
+// => 컴파일러는 이동계열의 함수를 제공하지 않습니다
+
+// => std::move() 사용시, 사용자가 만든 복사 생성자 호출. 
 
 struct Object
 {
@@ -24,8 +27,9 @@ int main()
 	Object o2; o2.name = "object#2";
 
 	Object o3 = o1;
-	Object o4 = std::move(o2);
+	Object o4 = std::move(o2); // 컴파일러가 만든 move 생성자 없으므로
+							   // 사용자가 만든 복사 생성자 사용
 
 	std::cout << o1.name << std::endl; // "object#1"
-	std::cout << o2.name << std::endl; // ""
+	std::cout << o2.name << std::endl; // "object#2"
 }
