ed4669626ec8025da877da1f3a3c7756198434fc
ecourse <smkang@codenuri.co.kr>
2024-12-11 16:09:33 +0900

    Add files via upload

--- DAY3/1_move16.cpp
+++ DAY3/1_move16.cpp
@@ -15,8 +15,8 @@ public:
 	// move 지원 setter 만들기
 
 	// 방법 #1. 2개의 setter 
-//	void set_name(const std::string& n) { name = n; }
-//	void set_name(std::string&& n) { name = std::move(n); }
+	void set_name(const std::string& n) { name = n; }
+	void set_name(std::string&& n) { name = std::move(n); }
 
 
 	// 방법 #2. T&& 를 사용하면 "방법#1" 을 자동생성할수 있습니다.
@@ -24,23 +24,32 @@ public:
 	void set_name(T&& n)
 	{
 		// 다음중 맞는 것은 ?
-		name = n;					// 1
-		name = std::move(n);		// 2
-		name = std::forward<T>(n);	// 3
+//		name = n;					// 1. 항상 복사
+//		name = std::move(n);		// 2. 항상 이동
+		name = std::forward<T>(n);	// 3. set_name(lvalue) 라면 복사
+									//    set_name(rvalue) 라면 이동
 	}
+
+	// 방법 #1 이 좋을까요 ? => 2개의 함수로 main 의 3가지 모두 해결 
+	// 방법 #2 가 좋을까요 ? => main 함수의 A, B, C 에 대해서 
+	//							3개의 set_name() 생성. 	
+	// 
+	
+	// 다양한 관점에서 "방법 #1" 이 좋습니다
+	// => 그런데, 인자가 2개 이상인 setter 라면!! - 다음 예제
 };
-
-
 
 int main()
 {
 	People p;
 
 	std::string s1 = "kim";
-	std::string s2 = "lee";
+	const std::string s2 = "kim";
+	std::string s3 = "lee";
 
-	p.set_name(s1);				
-	p.set_name(std::move(s2));	
+	p.set_name(s1);				// A	
+	p.set_name(s2);				// B
+	p.set_name(std::move(s3));	// C
 
 
 }
