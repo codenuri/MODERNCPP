fac743b9f73addad9d446ecf419aaa1cb84a9cb1
ecourse <smkang@codenuri.co.kr>
2024-12-11 13:54:00 +0900

    Add files via upload

--- DAY3/3_완벽한전달1.cpp
+++ DAY3/3_완벽한전달1.cpp
@@ -4,9 +4,13 @@
 void foo(int a)  {}
 void goo(int& a) { a = 100; }
 
+// forwarding : 전달 받은 인자를 다른 곳에 전달하는 것
+// perfect forwarding : forwarding 할때 어떠한 변화도 없이 그대도 전달하는 기술
+
+
 // 함수의 성능을 측정하는 함수
 template<typename F>
-void chronometry(F f, int arg)
+void chronometry(F f, const int& arg)
 {
 	// 시간 측정해서 기록해 놓고
 
