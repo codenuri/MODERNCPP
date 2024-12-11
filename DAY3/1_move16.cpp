11c8a4d4045213fccb91713d306cb6c29b79219e
ecourse <smkang@codenuri.co.kr>
2024-12-11 16:03:33 +0900

    Add files via upload

--- DAY3/1_move16.cpp
+++ DAY3/1_move16.cpp
@@ -15,11 +15,22 @@ public:
 	// move 지원 setter 만들기
 
 	// 방법 #1. 2개의 setter 
-	void set_name(const std::string& n) { name = n; }
-	void set_name(std::string&& n) { name = std::move(n); }
+//	void set_name(const std::string& n) { name = n; }
+//	void set_name(std::string&& n) { name = std::move(n); }
 
-	// 방법 #2.
+
+	// 방법 #2. T&& 를 사용하면 "방법#1" 을 자동생성할수 있습니다.
+	template<typename T>
+	void set_name(T&& n)
+	{
+		// 다음중 맞는 것은 ?
+		name = n;					// 1
+		name = std::move(n);		// 2
+		name = std::forward<T>(n);	// 3
+	}
 };
+
+
 
 int main()
 {
