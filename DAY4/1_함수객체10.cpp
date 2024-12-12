c464497d171c36f5cdab7cc0a58e96de5613c2a8
ecourse <smkang@codenuri.co.kr>
2024-12-12 09:40:17 +0900

    Add files via upload

---
+++ DAY4/1_함수객체10.cpp
@@ -0,0 +1,43 @@
+// chronometry.h 파일 추가 하세요 - git 에 있는 소스 복사 하세요
+// 1_함수객체10.cpp -
+#include <vector>
+#include <algorithm>
+#include "chronometry.h"
+
+const std::size_t count = 1000000;
+
+std::vector<int> v1;
+std::vector<int> v2;
+
+void init()
+{
+	// v1, v2 를 동일 데이타로 채움니다.
+	v1.reserve(count);
+	v2.reserve(count);
+
+	for (int i = 0; i < count; i++)
+	{
+		int n = rand();
+		v1.push_back(n);
+		v2.push_back(n);
+	}
+}
+
+void ex1()
+{
+	std::sort(v1.begin(), v1.end(), cmp1);
+	std::sort(v1.begin(), v1.end(), cmp2);
+}
+void ex2()
+{
+	std::sort(v2.begin(), v2.end(), Less{});
+	std::sort(v2.begin(), v2.end(), Greater{});
+}
+
+int main()
+{
+	init();
+
+	chronometry(ex1);
+	chronometry(ex2);
+}
