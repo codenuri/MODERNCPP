e403699fc8bf3566dc81aa8089f223a56476144d
ecourse <smkang@codenuri.co.kr>
2024-12-12 13:46:21 +0900

    Add files via upload

--- DAY4/4_람다기타3.cpp
+++ DAY4/4_람다기타3.cpp
@@ -18,4 +18,9 @@
 												b = std::move(tmp); };
 	swap2(n, d); // error. 1, 2개의 인자 타입이 같아야 하는데..
 				 //        다르기 때문에..
+
+	// 1, 2의 인자가 서로 다른 타입이 되게 하려면 아래의 2가지 방법 가능하고
+	// 결국 동일한 코드입니다.
+	auto f1 = [](auto a, auto b) {  return a + b; };
+	auto f2 = []<typename T1, typename T2>(T1 a, T2 b) {  return a + b; }
 }
