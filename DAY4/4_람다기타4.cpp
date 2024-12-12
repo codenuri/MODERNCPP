a2c167068acd6da04cd33eb43d4e1f11fd8b00e0
ecourse <smkang@codenuri.co.kr>
2024-12-12 13:49:43 +0900

    Add files via upload

---
+++ DAY4/4_람다기타4.cpp
@@ -0,0 +1,31 @@
+﻿// 약간 어려운 이야기 입니다.
+
+class CompilerGeneratedName
+{
+public:
+	inline int operator()(int a, int b) { return a + b; }
+};
+
+
+int main()
+{
+	int(*f)(int, int) = CompilerGeneratedName{};
+
+
+	// 람다 표현식을 함수 포인터에 담아서 사용할수 있습니다.
+//	int(*f)(int, int) = [](int a, int b) {return a + b; };
+						// 임시객체
+//	int n = f(1, 2);
+}
+
+
+
+
+
+
+
+
+
+
+
+
