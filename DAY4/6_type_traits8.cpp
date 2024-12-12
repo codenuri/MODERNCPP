d6800b7105e60bfacf02a80aa9a42656bba11ef8
ecourse <smkang@codenuri.co.kr>
2024-12-12 16:31:22 +0900

    Add files via upload

--- DAY4/6_type_traits8.cpp
+++ DAY4/6_type_traits8.cpp
@@ -15,11 +15,18 @@ T&& xmove(T& obj)
 	return static_cast<T&&>(obj);
 }
 
+
 int main()
 {
 	Object o1;
 
 	Object o2 = std::move(o1);	
+	Object o3 = std::move(Object{}); // ok
 
-	Object o3 = xmove(o2); 
+	Object o4 = xmove( o2 ); 
+//	Object o5 = xmove( Object{} );	// 되야 할까요 ? 안되야 할까요 ?
+									// 임시객체는 std::move() 없어도 move되지만
+									// std::move() 에 넣어도 되야 합니다.
+									// 현재는 error.
+									// 이유와 해결책은 다음소스에서
 }
