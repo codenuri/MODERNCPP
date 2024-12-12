acb4fbd078fe4b41f75abc81ab1c4b7e6232b6d8
ecourse <smkang@codenuri.co.kr>
2024-12-12 16:48:50 +0900

    Add files via upload

--- DAY4/6_type_traits7.cpp
+++ DAY4/6_type_traits7.cpp
@@ -2,10 +2,11 @@
 #include <type_traits>
 
 // 아래 한글부분 코드로 변경해 보세요 - std::is_pointer 사용
+// => std::is_pointer_v 가 편리합니다.
 
 template<typename T> void printv(const T& v)
 {
-	if (T 가 포인터 타입 이면)
+	if ( std::is_pointer_v<T> )
 		std::cout << v << " : " << *v << std::endl;
 	else
 		std::cout << v << std::endl;
