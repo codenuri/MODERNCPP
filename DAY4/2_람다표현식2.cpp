9b656f415fd072cd8969c6bf5fcdfe42ad2fa174
ecourse <smkang@codenuri.co.kr>
2024-12-12 10:41:16 +0900

    Add files via upload

--- DAY4/2_람다표현식2.cpp
+++ DAY4/2_람다표현식2.cpp
@@ -12,6 +12,7 @@ int main()
 
 	std::sort(v.begin(), v.end(), [](int a, int b) { return a < b; } );
 
+
 	// 위 한줄은 컴파일러에 의해 아래 처럼 변경됩니다.
 	class CompilerGeneratedName
 	{
@@ -24,8 +25,14 @@ int main()
 
 	std::sort(v.begin(), v.end(), CompilerGeneratedName{} );
 }
+// lambda expression
+// => 이름에서 알수 있듯 "expression(표현식)" 입니다.
+// => 표현식은 "한개의 값" 을 만드는 코드 집합..
+// => 람다 표현식이 만든 한개의 값은 "컴파일러가 생성한 클래스의 임시객체" 입니다.
 
-
+// 사실 C++에서는 lambda expression 문법이 없어도 됩니다.
+// => 사용자가 직접 함수 객체를 만들어서 사용하면 되므로!!
+// => 단지, 함수 객체를 편하게 만들기 위한 문법 입니다.
 
 
 
