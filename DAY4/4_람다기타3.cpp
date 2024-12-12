a9f6ffa231043cdfebd56f1ac63c81b7b631087a
ecourse <smkang@codenuri.co.kr>
2024-12-12 13:39:58 +0900

    Add files via upload

---
+++ DAY4/4_람다기타3.cpp
@@ -0,0 +1,12 @@
+﻿int main()
+{
+	// C++14 : generic lambda expression
+	auto swap = [](auto& a, auto& b) { auto tmp = std::move(a); 
+										   a = std::move(b); 
+										   b = std::move(tmp); };
+
+	int n = 10;
+	double d = 3.4;
+
+	swap(n, d); // 에러 나는게 좋을까요 ? 에러 없는게 좋을까요 ?
+}
