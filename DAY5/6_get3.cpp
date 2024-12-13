8812bcfc81347dbc43ca663ed084245fa6b269e7
ecourse <smkang@codenuri.co.kr>
2024-12-13 14:35:37 +0900

    Add files via upload

--- DAY5/6_get3.cpp
+++ DAY5/6_get3.cpp
@@ -2,15 +2,29 @@
 #include <iostream>
 #include "tuple.h"
 
+// #1. 사용하는 코드를 잘 생각해서, primary template 을 작성 합니다.
+template<int N, typename T> struct tuple_element
+{
+	// 튜플 T에서 N 번째 요소의 타입을 ? 에 표기해야 합니다.
+	// 그런데, 현재 코드에서는 알수 없습니다.
+	// using type = ? ;
+
+	// 그래서, 구할수 있도록 부분 특수화 기술을 사용해야 합니다.
+};
+
+
 template<typename T>
 void foo(T& tp)
 {
 	// T : tuple<int, double, short>
 
-	typename tuple_element<0, T>::type n1;	// <= 핵심
+	typename tuple_element<0, T>::type n1;	// <= 핵심  int n1
+//	typename tuple_element<1, T>::type n2;  // double n2
 
-	std::cout << typeid(n1).name() << std::endl;
+	std::cout << typeid(n1).name() << std::endl; // int
+//	std::cout << typeid(n2).name() << std::endl; // double
 }
+
 
 int main()
 {
