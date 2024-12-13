eb2d29e0d80c1e43911f8cd51588a1c14c50c1ee
ecourse <smkang@codenuri.co.kr>
2024-12-13 15:26:22 +0900

    Add files via upload

---
+++ DAY5/7_invoke2.cpp
@@ -0,0 +1,27 @@
+﻿class Object
+{
+public:
+	// Object obj;
+	// obj.f1(10) 의 원리는 컴파일러가
+	// f1(&obj, 10) 로 변경하는 것입니다.
+
+	void mf1(int code) {}  // void f1(Object* this, int code) {}
+
+	static void mf2(int code) {} // this 가 추가되지 않은 멤버 함수. 
+};
+
+void foo(int n) {}
+
+int main()
+{
+	void(*f1)(int) = &foo;		  
+	void(Object::*f2)(int) = &Object::mf1; 
+
+	Object obj;
+
+	// #1. 일반 함수 포인터와 멤버 함수 포인터는 사용법이 다릅니다.
+	f1(10);			// 일반 함수 포인터 사용
+	(obj.*f2)(10);	// 멤버 함수 포인터 사용
+
+	// #2. 위 문제를 해결하기 위해 C++ 창시자가 아래 문법을 제안했습니다.
+}
