bc8b5af121832356231cd006b6b3c7e54b519266
ecourse <smkang@codenuri.co.kr>
2024-12-13 10:01:40 +0900

    Add files via upload

--- DAY5/2_enable_if8.cpp
+++ DAY5/2_enable_if8.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 #include <type_traits>
 
+// enable_if 기술
+// => C++20 나오기 전까지 아주 널리 사용되었습니다.
+// => 정확히 이해 하면 그렇게 어려운 기술도 아닙니다. (SFINAE 를 잘 활용한것)
+// => 그런데, 코드가 복잡해 보입니다.
 
-template<typename T>
-std::enable_if_t< std::is_pointer_v<T> >
-printv(const T& v)
+// 그래서, C++20 에는 "requires clauses" 라는 문법이 도입됩니다.
+// => enable_if 라는 테크닉을 "문법으로 발전" 시킨것. 
+// => 아래 코드 빌드하려면 "C++버전 20" 으로 설정해야 합니다.
+
+template<typename T> requires std::is_pointer_v<T>
+void printv(const T& v)
 {
 	std::cout << "포인터인 경우\n";
 	std::cout << v << " : " << *v << std::endl;
 }
 
-template<typename T>
-std::enable_if_t< !std::is_pointer_v<T> >
-printv(const T& v)
+template<typename T> requires (!std::is_pointer_v<T>)
+void printv(const T& v)
 {
 	std::cout << "포인터가 아닌 경우\n";
 	std::cout << v << std::endl;
