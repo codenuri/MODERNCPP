50dc4bba89363b927fade12c4fe707ed96b21d9d
ecourse <smkang@codenuri.co.kr>
2024-12-13 10:47:17 +0900

    Add files via upload

--- DAY5/3_가변인자템플릿4.cpp
+++ DAY5/3_가변인자템플릿4.cpp
@@ -1,7 +1,6 @@
 ﻿#include <iostream>
 
 int  hoo(int a) { return -a; }
-
 void goo(int a, int b, int c) {}
 
 template<typename ... Types> void foo(Types ... args)
@@ -19,7 +18,13 @@ template<typename ... Types> void foo(Types ... args)
 	goo( args... );		// goo( 1, 2, 3 ) 의 의미
 	goo( (++args)...);	// goo( ++e1, ++e2, ++e3),  () 없어도 됩니다.
 
-	goo(hoo(args...));  // 어떻게 풀리는지 생각해 보세요
+//	goo(hoo(args...));  // 어떻게 풀리는지 생각해 보세요
+						// goo( hoo(e1, e2, e3)) 인데, hoo()는 인자가 한개이므로 error
+
+	goo( hoo(args)... ); // goo(hoo(e1), hoo(e2), hoo(e3)) 의 의미.. ok
+
+	// 핵심
+	// 위 예제를 통해서, ... 의 위치에 따른 차이점을 정확히 알아 두세요
 
 }
 
