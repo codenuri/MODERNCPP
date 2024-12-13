378b2d8bd7b303901a3e640c859129454a1a2472
ecourse <smkang@codenuri.co.kr>
2024-12-13 09:00:32 +0900

    Add files via upload

---
+++ DAY5/2_enable_if2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <type_traits>
+
+// 최대 공약수를 구하는 것을 생각해 봅시다.
+template<typename T> T gcd(T a, T b)
+{
+	static_assert(std::is_integral_v<T>, "error, not integer");
+
+	return 0;
+}
+
+int main()
+{
+	gcd(6, 9);
+	gcd(3.1,  5.4);
+}
