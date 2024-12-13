5fd04036e50fe1df248a6547e96378cc3a81575a
ecourse <smkang@codenuri.co.kr>
2024-12-13 14:48:21 +0900

    Add files via upload

---
+++ DAY5/6_get4.cpp
@@ -0,0 +1,53 @@
+// get3.cpp
+#include <iostream>
+#include "tuple.h"
+
+// #1. 사용하는 코드를 잘 생각해서, primary template 을 작성 합니다.
+template<int N, typename T> struct tuple_element
+{
+	// 튜플 T에서 N 번째 요소의 타입을 ? 에 표기해야 합니다.
+	// 그런데, 현재 코드에서는 알수 없습니다.
+	// using type = ? ;
+
+	// 그래서, 구할수 있도록 부분 특수화 기술을 사용해야 합니다.
+};
+
+// #1. N == 0
+template<typename T, typename ... Types>
+struct tuple_element<0, tuple<T, Types...> >
+{
+	// tuple<int, double, short> 의 0번째 요소 타입은 int
+	// tuple<int, double, short> 의 0번째 요소 를 저장하는 타입은 
+	using type = T;
+};
+
+
+// #2. N != 0
+template<int N, typename T, typename ... Types>
+struct tuple_element<N, tuple<T, Types...> >
+{
+	using type = typename tuple_element<N - 1, tuple< Types...>>::type;
+};
+
+
+template<typename T>
+void foo(T& tp)
+{
+	// T : tuple<int, double, short>
+
+	typename tuple_element<0, T>::type n1;	// <= 핵심  int n1
+	typename tuple_element<1, T>::type n2;  // double n2
+
+	std::cout << typeid(n1).name() << std::endl; // int
+	std::cout << typeid(n2).name() << std::endl; // double
+}
+
+
+int main()
+{
+	tuple<int, double, short> t(1, 3.4, 5);
+
+	foo(t);
+}
+
+
