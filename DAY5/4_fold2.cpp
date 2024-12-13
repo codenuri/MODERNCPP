5288ee57b62c4ddaaea7749ed00e8ef405e822c8
ecourse <smkang@codenuri.co.kr>
2024-12-13 11:55:18 +0900

    Add files via upload

--- DAY5/4_fold2.cpp
+++ DAY5/4_fold2.cpp
@@ -17,7 +17,7 @@ template<typename ... Types>
 void Show(Types ... args)
 {
 //	( printv(args)    ,        ... );
-	// pack이름    이항연산자    ...   => (args + ...) 의 형태이므로 nary  right fold 
+	// pack이름    이항연산자    ...   => (args + ...) 의 형태이므로 unary  right fold 
 	//									E1+(E2+(E3+(E4+E5)))
 	// printv(1) , ( printv(2), (printv(3), (printv(4) ,  ....... )
 	// => fold expresson 은 아주 많은 일을 할수 있습니다
@@ -29,7 +29,14 @@ void Show(Types ... args)
 
 
 	// 아래 코드가 하는일은 ?
-	( (std::cout << args << " ") , ... );
+	( (std::cout << args << " ")  ,  ... );
+	// pack 이름을사용하는표현색   op  ...   unary  right fold  => E1+(E2+(E3+(E4+E5)))
+	// (std::cout << 1 << " "), ((std::cout << 2 << " "), ((std::cout << 3 << " ")
+
+	// 핵심
+	// 1. pack 이름만 사용가능한것이 아니라 pack 이름을 포함한 표현식도 가능
+	// 2. 초기값은 0 같은 리터럴뿐아니라 std::cout 같은 객체도 가능
+	// 3. 이항연산자는 연산자 재정의 되어 있는 것도 활용가능 ( << 연산자등)
 
 }
 
