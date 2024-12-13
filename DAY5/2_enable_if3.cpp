fddafc9e3a5414af028aea6ca0d901d1701b9bba
ecourse <smkang@codenuri.co.kr>
2024-12-13 09:09:40 +0900

    Add files via upload

--- DAY5/2_enable_if3.cpp
+++ DAY5/2_enable_if3.cpp
@@ -1,16 +1,24 @@
 ﻿#include <iostream>
 #include <type_traits>
 
+// enable_if 를 사용한 반환 타입 만들기
+
+
+// 사용법 : enable_if< 조건, 반환타입 >::type
+// => 아래 코드는 결국 조건을 만족(정수계열 타입)하는 경우만 gcd 템플릿을
+//    사용하겠다는 것
 
 template<typename T> 
-
-std::enable_if< std::is_integral_v<T>, T>::type
-
+						//  true 라면			  T
+typename std::enable_if< std::is_integral_v<T>, T>::type
+						//  false 라면			 "::type" 없음. 
 gcd(T a, T b)
 {
 
 	return 0;
 }
+
+
 
 double gcd(double a, double b)
 {
