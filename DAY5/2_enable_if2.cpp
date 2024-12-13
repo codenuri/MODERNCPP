1e4d0bff2785e770e9da758abc4f8fa9eab56e23
ecourse <smkang@codenuri.co.kr>
2024-12-13 09:04:51 +0900

    Add files via upload

--- DAY5/2_enable_if2.cpp
+++ DAY5/2_enable_if2.cpp
@@ -1,6 +1,13 @@
 ﻿#include <iostream>
 #include <type_traits>
 
+// 핵심 
+// static_assert : 조건을 만족하지 않은 경우, 무조건 에러!!
+
+// 조건을 만족하지 못할때, 에러가 아니라
+// => 후보에서 제외 할수 없을까 ???
+// => SFINAE 와 enable_if 를 사용하면 됩니다.
+
 // 최대 공약수를 구하는 것을 생각해 봅시다.
 template<typename T> T gcd(T a, T b)
 {
@@ -9,8 +16,15 @@ template<typename T> T gcd(T a, T b)
 	return 0;
 }
 
+double gcd(double a, double b)
+{
+	std::cout << "double\n";
+	return 0;
+}
+
 int main()
 {
-	gcd(6, 9);
-	gcd(3.1,  5.4);
+	gcd(6, 9);		 // T
+	gcd(3.1,  5.4);	 // double 사용
+	gcd(3.1f, 5.4f); // T 버전 사용, static_assert 에서 실패. error
 }
