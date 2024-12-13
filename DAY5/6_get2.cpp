32b486aa78a2af12727572623aa327f841996bc2
ecourse <smkang@codenuri.co.kr>
2024-12-13 14:08:09 +0900

    Add files via upload

--- DAY5/6_get2.cpp
+++ DAY5/6_get2.cpp
@@ -21,5 +21,23 @@ int main()
 	std::cout << static_cast<tuple<double, short>&>(t).value << std::endl; // 3.4 나오게 해보세요
 
 	std::cout << static_cast<tuple<short>&>(t).value << std::endl; // 5 나오게 해보세요
+	
+	// 핵심 
+	// => 결국 tuple 이 보관한 각 요소에 접근하는 것은 캐스팅(컴파일 시간)일뿐입니다.
+	// => 따라서, 실행시 오버헤드는 없습니다.
+	
+	// 이제 get의 모양을 생각해 봅시다.
+
+	int n = get<0>(t);
+
+	get<0>(t) = 10;
 }
+
+template<>
+?
+get(? )
+{
+
+}
+
 
