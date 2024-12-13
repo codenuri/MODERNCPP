23d2240e53e7d4a2d4df2469e3fdbc33bddb07dd
ecourse <smkang@codenuri.co.kr>
2024-12-13 15:37:22 +0900

    Add files via upload

--- DAY5/7_invoke3.cpp
+++ DAY5/7_invoke3.cpp
@@ -13,12 +13,21 @@ void foo(int n, double d) {}
 template<typename F, typename ... T>
 decltype(auto) chronometry(F f, T&& ... args)
 {
-	return f(std::forward<T>(arg)...);
+	// stop_watch sw(true);
+
+//	return f(std::forward<T>(arg)...);  // 이렇게 하면 f 가 일반 함수 포인터는 문제 없지만
+										// 멤버 함수 포인터 라면 error 입니다.
+
+	return std::invoke(f, std::forward<T>(arg)...);
+				// 이렇게 해야, main 함수 처럼
+				// 일반함수, 멤버 함수가 모두 가능합니다.
 }
 
 int main()
 {
-	chronometry(&foo, 1, 3.4);
+	chronometry(&foo, 1, 3.4);	// foo(1, 3.4) 의 소요 시간 측정
 
-	chronometry(&Test::mf, 1, 3.4);
+	// 멤버 함수는 "함수 주소와 객체주소, 인자들"을 전달
+	Test test;
+	chronometry(&Test::mf, &test, 1, 3.4); // test.mf(1, 3.4) 의 소요시간 측정
 }
